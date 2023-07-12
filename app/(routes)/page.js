c1d9eade37cfc44eb3ae26624185a3b8c2de6e77
jonathankerth <jonathanpkerth@gmail.com>
2023-07-11 19:12:45 -0700

    add shopify buy dependency

--- app/(routes)/page.js
+++ app/(routes)/page.js
@@ -1,31 +1,15 @@
-import Link from "next/link";
-import Image from "next/image";
-import ProductCard from "./product/page.js";
+import { react } from "react";
 
-export default function Home({ products }) {
+<head></head>;
+
+export default function Home({}) {
 	return (
 		<div className="min-h-screen py-10 bg-gray-100">
 			<div className="container mx-auto px-5">
 				<h1 className="text-5xl font-bold text-center mb-10">
 					Oregonized Design
 				</h1>
-
-				<div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-5">
-					{products.map((product) => (
-						<ProductCard key={product.id} product={product} />
-					))}
-				</div>
 			</div>
 		</div>
 	);
-}
-
-export async function getStaticProps() {
-	const products = await getAllProducts();
-	return {
-		props: {
-			products,
-		},
-		revalidate: 60,
-	};
 }
