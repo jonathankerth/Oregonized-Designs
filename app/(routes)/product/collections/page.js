6792bd88a824739c41d139d8967dac63417a809a
jonathankerth <jonathanpkerth@gmail.com>
2023-07-13 18:03:23 -0700

    add logo to nav bar

---
+++ app/(routes)/product/collections/page.js
@@ -0,0 +1,13 @@
+"use client";
+import Footer from "../../../../components/footer.js";
+import Navbar from "../../../../components/navbar.js";
+
+export default function Collection({ Collection }) {
+	return (
+		<div>
+			<Navbar />
+			<div>Collections</div>
+			<Footer />
+		</div>
+	);
+}
