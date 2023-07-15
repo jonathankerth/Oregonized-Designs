a300e5410cecfe21abf87951a7b891c9da36c00d
jonathankerth <jonathanpkerth@gmail.com>
2023-07-15 15:38:41 -0700

    fix shopify import and dependencies

--- api/shopify.js
+++ api/shopify.js
@@ -1,4 +1,4 @@
-import { clients } from "@shopify/shopify-api";
+import { fetch } from "@shopify/shopify-api";
 
 // Initialize Shopify client
 const storefrontAccessToken = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN;
@@ -6,29 +6,30 @@ const storefrontAccessToken = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN;
 // Shop from which we're fetching data
 const shop = "oregonizeddesignco.myshopify.com";
 
-// StorefrontClient takes in the shop url and the Storefront Access Token for that shop.
-const storefrontClient = new clients.Storefront({
-	domain: shop,
-	storefrontAccessToken,
-});
-
 // Define API route handler
 export default async function shopifyAPIHandler(req, res) {
 	// Handle the API request
 	try {
-		// Use client.query and pass your query as `data`
-		const products = await storefrontClient.query({
-			data: `{
-				products (first: 3) {
-					edges {
-						node {
-							id
-							title
-						}
-					}
-				}
-			}`,
+		// Make the API request to fetch products
+		const response = await fetch({
+			query: `
+        {
+          products(first: 3) {
+            edges {
+              node {
+                id
+                title
+              }
+            }
+          }
+        }
+      `,
+			storefrontAccessToken,
+			url: `https://${shop}/api/2021-07/graphql.json`,
 		});
+
+		const data = await response.json();
+		const products = data.data.products.edges.map((edge) => edge.node);
 
 		// Return the response
 		res.status(200).json({ products });
