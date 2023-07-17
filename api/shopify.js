e1b76ef7e65b64cf7359e7d19dc959d7c07f3b52
jonathankerth <jonathanpkerth@gmail.com>
2023-07-16 17:06:04 -0700

    remove hardcoded item placeholders

--- api/shopify.js
+++ api/shopify.js
@@ -1,40 +1,56 @@
-import { fetch } from "@shopify/shopify-api";
+import { fetch } from '@shopify/shopify-api'
 
 // Initialize Shopify client
-const storefrontAccessToken = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+const storefrontAccessToken = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN
 
 // Shop from which we're fetching data
-const shop = "oregonizeddesignco.myshopify.com";
+const shop = 'oregonizeddesignco.myshopify.com'
 
 // Define API route handler
 export default async function shopifyAPIHandler(req, res) {
-	// Handle the API request
-	try {
-		// Make the API request to fetch products
-		const response = await fetch({
-			query: `
-        {
-          products(first: 3) {
-            edges {
-              node {
-                id
-                title
-              }
-            }
-          }
-        }
-      `,
-			storefrontAccessToken,
-			url: `https://${shop}/api/2021-07/graphql.json`,
-		});
+  // Handle the API request
+  try {
+    // Make the API request to fetch products
+    const response = await fetch({
+      query: `
+			{
+			  products(first: 3) {
+				edges {
+				  node {
+					id
+					title
+					description
+					images(first: 1) {
+					  edges {
+						node {
+						  originalSrc
+						  altText
+						}
+					  }
+					}
+					priceRange {
+					  maxVariantPrice {
+						amount
+						currencyCode
+					  }
+					}
+					totalInventory
+				  }
+				}
+			  }
+			}
+		  `,
+      storefrontAccessToken,
+      url: `https://${shop}/api/2021-07/graphql.json`,
+    })
 
-		const data = await response.json();
-		const products = data.data.products.edges.map((edge) => edge.node);
+    const data = await response.json()
+    const products = data.data.products.edges.map((edge) => edge.node)
 
-		// Return the response
-		res.status(200).json({ products });
-	} catch (error) {
-		// Handle errors
-		res.status(500).json({ error: "An error occurred" });
-	}
+    // Return the response
+    res.status(200).json({ products })
+  } catch (error) {
+    // Handle errors
+    res.status(500).json({ error: 'An error occurred' })
+  }
 }
