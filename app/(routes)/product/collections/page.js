ea96a957fcf8b6593c5e012fbc39119aaabf4aea
jonathankerth <jonathanpkerth@gmail.com>
2023-07-19 23:05:07 -0700

    add working product import to front page

--- app/(routes)/product/collections/page.js
+++ app/(routes)/product/collections/page.js
@@ -1,37 +1,49 @@
 'use client'
 import { useEffect, useState } from 'react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const GRAPHQL_ENDPOINT =
   'https://oregonizeddesignco.myshopify.com/api/2023-07/graphql.json'
-const STOREFRONT_ACCESS_TOKEN = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN
+
+const STOREFRONT_ACCESS_TOKEN =
+  process.env.NEXT_PUBLIC_SHOPIFY_STORE_FRONT_ACCESS_TOKEN
 
 function ProductsPage() {
-  const [products, setProducts] = useState([])
+  const [collections, setCollections] = useState([])
   const [error, setError] = useState(null)
 
   useEffect(() => {
-    async function fetchProducts() {
-      const query = `
-      {
-        products(first: 10) {
-          edges {
-            node {
+    async function fetchCollections() {
+      const collectionQuery = `
+        query getCollections {
+          collections(first: 10) {
+            edges {
+              node {
+                id
+                handle
+              }
+            }
+          }
+        }
+      `
+
+      const productQuery = `
+        query getProducts($collectionIds: [ID!]!) {
+          nodes(ids: $collectionIds) {
+            ... on Collection {
               id
               title
-              description
-              onlineStoreUrl
-              images(first: 1) {
+              products(first: 10) {
                 edges {
                   node {
-                    src
-                  }
-                }
-              }
-              variants(first: 1) {
-                edges {
-                  node {
-                    priceV2 {
-                      amount
+                    id
+                    title
+                    priceRange {
+                      minVariantPrice {
+                        amount
+                      }
                     }
                   }
                 }
@@ -39,57 +51,93 @@ function ProductsPage() {
             }
           }
         }
-      }`
+      `
 
       console.log('About to send request to Shopify...')
 
       try {
-        const response = await fetch(GRAPHQL_ENDPOINT, {
+        // Fetch collections
+        const collectionResponse = await fetch(GRAPHQL_ENDPOINT, {
           method: 'POST',
           headers: {
             'X-Shopify-Storefront-Access-Token': STOREFRONT_ACCESS_TOKEN,
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ query }),
+          body: JSON.stringify({ query: collectionQuery }),
         })
 
-        console.log('Received response from Shopify:', response)
+        const collectionJsonResponse = await collectionResponse.json()
 
-        const jsonResponse = await response.json()
-
-        console.log('JSON response:', jsonResponse)
-
-        if (!response.ok || jsonResponse.errors) {
-          const errorMessage = jsonResponse.errors
-            ? jsonResponse.errors[0].message
+        if (!collectionResponse.ok || collectionJsonResponse.errors) {
+          const errorMessage = collectionJsonResponse.errors
+            ? collectionJsonResponse.errors[0].message
             : 'Network response was not ok'
           throw new Error(errorMessage)
         }
 
-        setProducts(jsonResponse.data.products.edges.map((edge) => edge.node))
+        // Extract collection IDs
+        const collectionIds = collectionJsonResponse.data.collections.edges.map(
+          (edge) => edge.node.id
+        )
+
+        // Fetch products for each collection
+        const productResponse = await fetch(GRAPHQL_ENDPOINT, {
+          method: 'POST',
+          headers: {
+            'X-Shopify-Storefront-Access-Token': STOREFRONT_ACCESS_TOKEN,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            query: productQuery,
+            variables: { collectionIds },
+          }),
+        })
+
+        const productJsonResponse = await productResponse.json()
+
+        if (!productResponse.ok || productJsonResponse.errors) {
+          const errorMessage = productJsonResponse.errors
+            ? productJsonResponse.errors[0].message
+            : 'Network response was not ok'
+          throw new Error(errorMessage)
+        }
+
+        // Combine collection and product data
+        const collectionsWithData =
+          collectionJsonResponse.data.collections.edges.map((edge) => {
+            const collectionData = edge.node
+            const products = productJsonResponse.data.nodes
+              .find((node) => node.id === collectionData.id)
+              .products.edges.map((edge) => edge.node)
+            return { ...collectionData, products }
+          })
+
+        setCollections(collectionsWithData)
       } catch (err) {
-        console.error('There was a problem fetching products:', err)
+        console.error('There was a problem fetching collections:', err)
         setError(err.message)
       }
     }
 
-    fetchProducts()
+    fetchCollections()
   }, [])
 
   if (error) {
-    return <div>Error loading products: {error}</div>
+    return <div>Error loading collections: {error}</div>
   }
 
   return (
     <div>
-      <h1 className="mt-6 mb-6">Products</h1>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.title}</h2>
-          <img src={product.images.edges[0]?.node.src} alt={product.title} />
-          <p>{product.description}</p>
-          <p>${product.variants.edges[0]?.node.priceV2.amount}</p>
-          <a href={product.onlineStoreUrl}>View Product</a>
+      <h1 className="mt-6 mb-6">Collections</h1>
+      {collections.map((collection) => (
+        <div key={collection.id}>
+          <h2>{collection.handle}</h2>
+          {collection.products.map((product) => (
+            <div key={product.id}>
+              <h3>{product.title}</h3>
+              <p>Price: {product.priceRange.minVariantPrice.amount}</p>
+            </div>
+          ))}
         </div>
       ))}
     </div>
