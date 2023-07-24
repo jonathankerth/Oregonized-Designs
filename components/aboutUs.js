94ba870da841eb0887c537c3dc90adcb8d3e9285
jonathankerth <jonathanpkerth@gmail.com>
2023-07-23 23:37:25 -0700

    update about front page

--- components/aboutUs.js
+++ components/aboutUs.js
@@ -1,20 +1,21 @@
 export default function AboutUs() {
-	return (
-		<div className="bg-gray-500/75 py-24 sm:py-32 w-full">
-			<div className="mx-auto max-w-7xl px-6 lg:px-8">
-				<div className="mx-auto max-w-2xl lg:mx-0">
-					<h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-						About Us
-					</h2>
-					<p className="mt-6 text-lg leading-8 text-gray-300">
-						Oregonized Design is a dropshipping company focused on providing
-						high-quality products designed in the unique and beautiful aesthetic
-						inspired by the state of Oregon. Our mission is to share the beauty
-						of Orego&apos;s natural landscapes and vibrant cityscapes through
-						our designs.
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-gray-500/75 py-24 sm:py-32 w-full">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            About Us
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            Oregonized Design Co. is dedicated to offering high-quality products
+            that capture the unique and beautiful aesthetic inspired by the
+            state of Oregon. Our mission is to celebrate and share the beauty of
+            Oregon&apos;s natural landscapes and vibrant cityscapes through our
+            designs. Oregonized Design Co.is a small business based in Portland,
+            created by Sidney Gallardo and Jonathan Kerth.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
