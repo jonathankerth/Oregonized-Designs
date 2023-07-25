d6114b960ae52efa3d7ab34215dab6411b4236e5
jonathankerth <jonathanpkerth@gmail.com>
2023-07-24 23:05:32 -0700

    add checkout page

---
+++ app/(routes)/cart/checkout/page.js
@@ -0,0 +1,87 @@
+'use client'
+import { useEffect, useState } from 'react'
+
+const GRAPHQL_ENDPOINT =
+  'https://oregonizeddesignco.myshopify.com/api/2023-07/graphql.json'
+
+const STOREFRONT_ACCESS_TOKEN =
+  process.env.NEXT_PUBLIC_SHOPIFY_STORE_FRONT_ACCESS_TOKEN
+
+const Checkout = () => {
+  const [cart, setCart] = useState(null)
+  const cartId = window.location.search.replace('?cartId=', '')
+
+  useEffect(() => {
+    async function fetchCartDetails() {
+      if (!cartId) {
+        return // Don't fetch cart details if cartId is not available
+      }
+
+      const cartQuery = `
+        query getCart($cartId: ID!) {
+          cart(id: $cartId) {
+            // ... (rest of your query remains the same)
+          }
+        }
+      `
+
+      try {
+        const response = await fetch(GRAPHQL_ENDPOINT, {
+          method: 'POST',
+          headers: {
+            'X-Shopify-Storefront-Access-Token': STOREFRONT_ACCESS_TOKEN,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            query: cartQuery,
+            variables: { cartId },
+          }),
+        })
+
+        const jsonResponse = await response.json()
+        setCart(jsonResponse.data.cart)
+      } catch (error) {
+        console.error('Error fetching cart details:', error)
+      }
+    }
+
+    fetchCartDetails()
+  }, [cartId])
+
+  if (!cart && cartId) {
+    // Display loading or an error message
+    return <div>Loading...</div>
+  }
+
+  const handleContinueShopping = () => {
+    // Replace this URL with the page URL where you want to continue shopping.
+    // For example: '/products' or '/'
+    window.location.href = '/'
+  }
+
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:px-0">
+        <h1 className="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Checkout Page
+        </h1>
+
+        {/* Display cart details and checkout information here */}
+        {/* ... */}
+        {/* (You can use the cart state to access the cart details and display them on the page) */}
+
+        <div className="mt-10">
+          <button
+            type="button"
+            onClick={handleContinueShopping}
+            className="w-full rounded-md border border-transparent bg-indigo-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+          >
+            Continue Shopping
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Checkout
--- app/(routes)/cart/page.js
+++ app/(routes)/cart/page.js
@@ -1,35 +1,106 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/20/solid'
+'use client'
+import { useEffect, useState } from 'react'
+import { CheckIcon, ClockIcon } from '@heroicons/react/outline'
 
-const products = [
-  {
-    id: 1,
-    name: 'Artwork Tee',
-    href: '#',
-    price: '$32.00',
-    color: 'Mint',
-    size: 'Medium',
-    inStock: true,
-    imageSrc:
-      'https://tailwindui.com/img/ecommerce-images/checkout-page-03-product-04.jpg',
-    imageAlt: 'Front side of mint cotton t-shirt with wavey lines pattern.',
-  },
-  {
-    id: 2,
-    name: 'Basic Tee',
-    href: '#',
-    price: '$32.00',
-    color: 'Charcoal',
-    inStock: false,
-    leadTime: '7-8 years',
-    size: 'Large',
-    imageSrc:
-      'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-02.jpg',
-    imageAlt: 'Front side of charcoal cotton t-shirt.',
-  },
-  // More products...
-]
+// Set your GraphQL endpoint and storefront access token
+const GRAPHQL_ENDPOINT =
+  'https://oregonizeddesignco.myshopify.com/api/2023-07/graphql.json'
+const STOREFRONT_ACCESS_TOKEN =
+  process.env.NEXT_PUBLIC_SHOPIFY_STORE_FRONT_ACCESS_TOKEN
 
-export default function Example() {
+export default function Cart() {
+  const [cart, setCart] = useState(null)
+
+  useEffect(() => {
+    async function fetchCartDetails() {
+      const cartId = 'YOUR_CART_ID' // Replace with your actual cart ID
+
+      const cartQuery = `
+        query getCart($cartId: ID!) {
+          cart(id: $cartId) {
+            id
+            attributes {
+              key
+              value
+            }
+            buyerIdentity {
+              email
+              phone
+            }
+            checkoutUrl
+            cost {
+              totalAmount
+              currencyCode
+            }
+            createdAt
+            discountAllocations {
+              allocatedAmount
+              discountCode
+            }
+            discountCodes
+            note
+            totalQuantity
+            updatedAt
+            lines(first: 10) {
+              edges {
+                node {
+                  id // Add the ID field for product identification
+                  title
+                  variant {
+                    priceV2 {
+                      amount
+                      currencyCode
+                    }
+                    image {
+                      src
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `
+
+      try {
+        const response = await fetch(GRAPHQL_ENDPOINT, {
+          method: 'POST',
+          headers: {
+            'X-Shopify-Storefront-Access-Token': STOREFRONT_ACCESS_TOKEN,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            query: cartQuery,
+            variables: { cartId },
+          }),
+        })
+
+        const jsonResponse = await response.json()
+        setCart(jsonResponse.data.cart)
+      } catch (error) {
+        console.error('Error fetching cart details:', error)
+      }
+    }
+
+    fetchCartDetails()
+  }, [])
+
+  const products = cart?.lines?.edges?.map((edge) => edge.node) || []
+
+  const handleRemoveFromCart = async (productId) => {
+    const updatedProducts = products.filter(
+      (product) => product.id !== productId
+    )
+    setCart({
+      ...cart,
+      lines: { edges: updatedProducts.map((product) => ({ node: product })) },
+    })
+  }
+
+  const handleCheckout = () => {
+    window.location.href = '/cart/checkout'
+  }
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:px-0">
@@ -51,8 +122,8 @@ export default function Example() {
                 <li key={product.id} className="flex py-6">
                   <div className="flex-shrink-0">
                     <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
+                      src={product.variant.image.src}
+                      alt={product.title}
                       className="h-24 w-24 rounded-md object-cover object-center sm:h-32 sm:w-32"
                     />
                   </div>
@@ -65,47 +136,46 @@ export default function Example() {
                             href={product.href}
                             className="font-medium text-gray-700 hover:text-gray-800"
                           >
-                            {product.name}
+                            {product.title}
                           </a>
                         </h4>
                         <p className="ml-4 text-sm font-medium text-gray-900">
-                          {product.price}
+                          {product.variant.priceV2.amount}{' '}
+                          {product.variant.priceV2.currencyCode}
                         </p>
                       </div>
                       <p className="mt-1 text-sm text-gray-500">
-                        {product.color}
-                      </p>
-                      <p className="mt-1 text-sm text-gray-500">
-                        {product.size}
+                        {product.attributes?.value}
                       </p>
                     </div>
 
                     <div className="mt-4 flex flex-1 items-end justify-between">
                       <p className="flex items-center space-x-2 text-sm text-gray-700">
                         {product.inStock ? (
-                          <CheckIcon
-                            className="h-5 w-5 flex-shrink-0 text-green-500"
-                            aria-hidden="true"
-                          />
+                          <>
+                            In stock
+                            <CheckIcon
+                              className="h-5 w-5 flex-shrink-0 text-green-500"
+                              aria-hidden="true"
+                            />
+                          </>
                         ) : (
-                          <ClockIcon
-                            className="h-5 w-5 flex-shrink-0 text-gray-300"
-                            aria-hidden="true"
-                          />
+                          <>
+                            Will ship in {product.leadTime}
+                            <ClockIcon
+                              className="h-5 w-5 flex-shrink-0 text-gray-300"
+                              aria-hidden="true"
+                            />
+                          </>
                         )}
-
-                        <span>
-                          {product.inStock
-                            ? 'In stock'
-                            : `Will ship in ${product.leadTime}`}
-                        </span>
                       </p>
                       <div className="ml-4">
                         <button
                           type="button"
+                          onClick={() => handleRemoveFromCart(product.id)}
                           className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
                         >
-                          <span>Remove</span>
+                          Remove
                         </button>
                       </div>
                     </div>
@@ -139,7 +209,8 @@ export default function Example() {
 
             <div className="mt-10">
               <button
-                type="submit"
+                type="button"
+                onClick={handleCheckout}
                 className="w-full rounded-md border border-transparent bg-indigo-600 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
               >
                 Checkout
