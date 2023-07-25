be01ad83319d47ced3101f83bff26dd4161611d4
jonathankerth <jonathanpkerth@gmail.com>
2023-07-25 15:21:55 -0700

    add product list page and start all product page

--- app/(routes)/product/page.js
+++ app/(routes)/product/page.js
@@ -1,13 +1,98 @@
-"use client";
-import Footer from "../../../components/footer.js";
-import Navbar from "../../../components/navbar.js";
+'use client'
+import { useState, useEffect } from 'react'
+import Image from 'next/image'
+import BuyButton from '../../../components/buyButton'
 
-export default function Sale({ sale }) {
-	return (
-		<div>
-			<Navbar />
-			<div>products</div>
-			<Footer />
-		</div>
-	);
+const GRAPHQL_ENDPOINT =
+  'https://oregonizeddesignco.myshopify.com/api/2023-07/graphql.json'
+
+const STOREFRONT_ACCESS_TOKEN =
+  process.env.NEXT_PUBLIC_SHOPIFY_STORE_FRONT_ACCESS_TOKEN
+
+function Product({ productId }) {
+  const [product, setProduct] = useState(null)
+
+  useEffect(() => {
+    async function fetchProductData() {
+      try {
+        const productQuery = `
+          query getProduct($id: ID!) {
+            node(id: $id) {
+              ... on Product {
+                id
+                title
+                images(first: 1) {
+                  edges {
+                    node {
+                      src
+                    }
+                  }
+                }
+                priceRange {
+                  minVariantPrice {
+                    amount
+                  }
+                }
+              }
+            }
+          }
+        `
+
+        const response = await fetch(GRAPHQL_ENDPOINT, {
+          method: 'POST',
+          headers: {
+            'X-Shopify-Storefront-Access-Token': STOREFRONT_ACCESS_TOKEN,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            query: productQuery,
+            variables: { id: productId },
+          }),
+        })
+
+        const jsonResponse = await response.json()
+
+        if (!response.ok || jsonResponse.errors) {
+          const errorMessage = jsonResponse.errors
+            ? jsonResponse.errors[0].message
+            : 'Network response was not ok'
+          throw new Error(errorMessage)
+        }
+
+        setProduct(jsonResponse.data.node)
+      } catch (err) {
+        console.error('There was a problem fetching the product:', err)
+      }
+    }
+
+    if (productId) {
+      fetchProductData()
+    }
+  }, [productId])
+
+  if (!product) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <h1 className="text-4xl font-bold mt-6 mb-6">{product.title}</h1>
+      <div className="flex-none w-full border-t mt-4 pt-4">
+        <Image
+          src={product.images.edges[0].node.src}
+          alt={product.title}
+          width={640}
+          height={480}
+          className="w-full h-64 object-cover mb-4"
+        />
+        <h3 className="text-2xl font-medium">{product.title}</h3>
+        <p className="text-gray-600 mt-2">
+          Price: ${product.priceRange.minVariantPrice.amount}
+        </p>
+        <BuyButton productId={product.id} />
+      </div>
+    </div>
+  )
 }
+
+export default Product
---
+++ app/(routes)/product/productList/page.js
@@ -0,0 +1,171 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
+import dotenv from 'dotenv'
+import BuyButton from '../../../../components/buyButton'
+
+dotenv.config()
+
+const GRAPHQL_ENDPOINT =
+  'https://oregonizeddesignco.myshopify.com/api/2023-07/graphql.json'
+
+const STOREFRONT_ACCESS_TOKEN =
+  process.env.NEXT_PUBLIC_SHOPIFY_STORE_FRONT_ACCESS_TOKEN
+
+function ProductsPage() {
+  const [collections, setCollections] = useState([])
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    async function fetchCollections() {
+      const collectionQuery = `
+        query getCollections {
+          collections(first: 10) {
+            edges {
+              node {
+                id
+                handle
+              }
+            }
+          }
+        }
+      `
+
+      const productQuery = `
+      query getProducts($collectionIds: [ID!]!) {
+        nodes(ids: $collectionIds) {
+          ... on Collection {
+            id
+            title
+            products(first: 10) {
+              edges {
+                node {
+                  id
+                  title
+                  images(first: 1) {
+                    edges {
+                      node {
+                        src
+                      }
+                    }
+                  }
+                  priceRange {
+                    minVariantPrice {
+                      amount
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+
+      console.log('About to send request to Shopify...')
+
+      try {
+        // Fetch collections
+        const collectionResponse = await fetch(GRAPHQL_ENDPOINT, {
+          method: 'POST',
+          headers: {
+            'X-Shopify-Storefront-Access-Token': STOREFRONT_ACCESS_TOKEN,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ query: collectionQuery }),
+        })
+
+        const collectionJsonResponse = await collectionResponse.json()
+
+        if (!collectionResponse.ok || collectionJsonResponse.errors) {
+          const errorMessage = collectionJsonResponse.errors
+            ? collectionJsonResponse.errors[0].message
+            : 'Network response was not ok'
+          throw new Error(errorMessage)
+        }
+
+        // Extract collection IDs
+        const collectionIds = collectionJsonResponse.data.collections.edges.map(
+          (edge) => edge.node.id
+        )
+
+        // Fetch products for each collection
+        const productResponse = await fetch(GRAPHQL_ENDPOINT, {
+          method: 'POST',
+          headers: {
+            'X-Shopify-Storefront-Access-Token': STOREFRONT_ACCESS_TOKEN,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            query: productQuery,
+            variables: { collectionIds },
+          }),
+        })
+
+        const productJsonResponse = await productResponse.json()
+
+        if (!productResponse.ok || productJsonResponse.errors) {
+          const errorMessage = productJsonResponse.errors
+            ? productJsonResponse.errors[0].message
+            : 'Network response was not ok'
+          throw new Error(errorMessage)
+        }
+
+        // Combine collection and product data
+        const collectionsWithData =
+          collectionJsonResponse.data.collections.edges.map((edge) => {
+            const collectionData = edge.node
+            const products = productJsonResponse.data.nodes
+              .find((node) => node.id === collectionData.id)
+              .products.edges.map((edge) => edge.node)
+            return { ...collectionData, products }
+          })
+
+        setCollections(collectionsWithData)
+      } catch (err) {
+        console.error('There was a problem fetching collections:', err)
+        setError(err.message)
+      }
+    }
+
+    fetchCollections()
+  }, [])
+
+  if (error) {
+    return <div>Error loading collections: {error}</div>
+  }
+
+  return (
+    <div className="bg-gray-100 p-2">
+      <h1 className="text-4xl font-bold mt-6 mb-6">New Items</h1>
+      {collections.map((collection) => (
+        <div
+          key={collection.id}
+          className="bg-white p-4 my-4 rounded shadow-lg"
+        >
+          <div className="flex overflow-x-auto space-x-4">
+            {collection.products.map((product) => (
+              <div
+                key={product.id}
+                className="flex-none w-full border-t mt-4 pt-4"
+              >
+                <img
+                  src={product.images.edges[0].node.src}
+                  alt={product.title}
+                  className="w-full h-64 object-cover mb-4"
+                />
+                <h3 className="text-2xl font-medium">{product.title}</h3>
+                <p className="text-gray-600 mt-2">
+                  Price: ${product.priceRange.minVariantPrice.amount}
+                </p>
+                <BuyButton productId={product.id} />
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ProductsPage
